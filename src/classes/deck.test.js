607df9302b538c0654de8166cc390afe335f3182
Fabio Saito <fabio.h.saito@gmail.com>
2022-03-09 13:29:24 -0300

    improve deck.test.js

--- src/classes/deck.test.js
+++ src/classes/deck.test.js
@@ -2,21 +2,27 @@ import {Deck} from "./deck"
 
 describe ("Deck", ()=>{
   const deck = new Deck()
-  it("#generateDeck", ()=>{
-    const DECK_SIZE = 52
-    expect(deck.current_deck).toHaveLength(DECK_SIZE)
+  describe("#generateDeck", ()=>{
+    it("creates a shuffled deck of cards with 52 items", ()=>{
+      const DECK_SIZE = 52
+      expect(deck.current_deck).toHaveLength(DECK_SIZE)
+    })
   })
 
-  it("#shuffleCurrentDeck", ()=>{
-    deck.shuffleCurrentDeck()
-    const shuffledDeck = [...deck.current_deck]
-    expect(shuffledDeck).toEqual(deck.current_deck)
-    expect(shuffledDeck).not.toBe(deck.current_deck)
+  describe("#shuffleCurrentDeck", ()=>{
+    it("shuffles current deck", ()=>{
+      deck.shuffleCurrentDeck()
+      const shuffledDeck = [...deck.current_deck]
+      expect(shuffledDeck).toEqual(deck.current_deck)
+      expect(shuffledDeck).not.toBe(deck.current_deck)
+    })
   })
 
-  it("#getRandomDeck", ()=>{
-    const magicDeck = deck.getRandomDeck(21)
-    expect(magicDeck).toHaveLength(21)
-    expect(deck.current_deck).toEqual(expect.not.arrayContaining(magicDeck))
+  describe("#getRandomDeck", ()=>{
+    it("picks random cards from current deck", ()=>{
+      const magicDeck = deck.getRandomDeck(21)
+      expect(magicDeck).toHaveLength(21)
+      expect(deck.current_deck).toEqual(expect.not.arrayContaining(magicDeck))
+    })
   })
 })
