4037a8bfdf20ba6eec749927e9dd0447201910c4
Fabio Saito <fabio.h.saito@gmail.com>
2022-03-10 09:48:11 -0300

    apply pattern to test

--- src/utils/createDeck.test.js
+++ src/utils/createDeck.test.js
@@ -1,6 +1,6 @@
 import {createDeck} from "./createDeck"
 
-describe ("magic deck", ()=>{
+describe ("createDeck", ()=>{
   it("creates a deck with 21 random cards", ()=>{
     const deck = createDeck()
     const MAGIC_DECK_SIZE = 21
