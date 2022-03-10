3b632bba1bfe096b2fca736460c36eb846e89622
Fabio Saito <fabio.h.saito@gmail.com>
2022-03-10 17:12:42 -0300

    insert CardTrick component to App.js

--- src/App.js
+++ src/App.js
@@ -1,48 +1,11 @@
-import { ShowCards } from "./components/ShowCards.js"
-import { createDeck } from "./utils/createDeck.js"
 import "./App.css"
-import { useState } from "react"
+import { CardTrick } from "./components/CardTrick.js"
 
 function App() {
-  const NUMBER_OF_ROWS = 3
-  const MIDDLE_OF_ARRAY_INDEX = 7
-
-  const magicDeck = createDeck()
-
-  function convertToSlicedArray(arr, slices){
-    return Array(slices).fill().map( (_,index) => cardFilterByMod(arr, index) )
-  }
-  const [magicDeckSlices, setMagicDeckSlices] = useState(convertToSlicedArray(magicDeck, NUMBER_OF_ROWS))
-  
-  function cardFilterByMod(sourceDeck, mod) {
-    return sourceDeck.filter( (_, index) => index % 3 === mod )
-  }
-
-  function handleMagicTrick(originalDeck, choice) {
-    const newDeck = [...originalDeck].flat().filter((el) => !originalDeck[choice].includes(el))
-    newDeck.splice(MIDDLE_OF_ARRAY_INDEX, 0, ...originalDeck[choice])
-    return convertToSlicedArray(newDeck, originalDeck.length)
-  }
-
-  const handleMagicDeck = (choice) => {
-    setMagicDeckSlices((prevMagicDeck) => handleMagicTrick(prevMagicDeck, choice));
-};
-
   return (
     <div className="container">
       <p className="page__title"> 11Th Card Trick </p>
-
-      <div className="deck__container" onClick={()=>{handleMagicDeck(0)}}>
-        <ShowCards deck = {magicDeckSlices[0]} />
-      </div>
-
-      <div className="deck__container" onClick={()=>{handleMagicDeck(1)}}>  
-        <ShowCards deck = {magicDeckSlices[1]} />
-      </div> 
-    
-      <div className="deck__container" onClick={()=>{handleMagicDeck(2)}}>  
-        <ShowCards deck = {magicDeckSlices[2]} />
-      </div>
+      <CardTrick/>
     </div>
   )
 }
--- src/App.test.js
+++ src/App.test.js
@@ -3,6 +3,6 @@ import App from './App'
 
 test('renders learn react link', () => {
   render(<App />)
-  const linkElement = screen.getByText(/DECK/i)
+  const linkElement = screen.getByText(/11Th Card Trick/i)
   expect(linkElement).toBeInTheDocument()
 })
